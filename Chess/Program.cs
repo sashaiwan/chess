be0fbfd46047ef50677a65f8ed06a027748c135d
sashiwan <samueliwanczuk@gmail.com>
2022-09-25 14:36:56 -0300

    code for debuggins purposes

--- Chess/Program.cs
+++ Chess/Program.cs
@@ -1,73 +1,24 @@
 ﻿using Chess.Piece;
 using Chess.Board;
+using Chess.Services;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Piece pawn = new Pawn("pawn", new Coordinate(0, 0));
-        pawn.GetCoords();
-        pawn.Move(new Coordinate(0, 3));
-        pawn.GetCoords();
+        BoardBase board = new(4);
+        PieceService pieceService = new();
+        PositionController positionController = new(board, pieceService);
+
+        var allSquares = board.GetAllSquares();
+        Console.WriteLine(String.Join(", ", allSquares));
+        Console.WriteLine(allSquares.Count);
+
+        List<(int, int)> positionSet = positionController.GetPositionSet();
+        Console.WriteLine(String.Join(", ", positionSet));
+        Console.WriteLine(positionSet.Count);
+        //Console.WriteLine(String.Join(", ", positionSet));
+
         Console.ReadLine();
     }
-
-    static void Draw()
-    { 
-        Console.SetCursorPosition(0, 0);
-        Console.Write("╔");
-
-    }
-
-    static void Render()
-    {
-        int width = 20;
-        string s = "╔";
-
-        for (int i = 0; i < width; i++)
-        {
-            s += "══";
-
-            if (i == width - 1)
-            {
-                s += "╗" + "\n";
-            } else
-            {
-                s += "╦";
-            }
-        }
-
-        for (int i = 0; i < width + 1; i++)
-        {
-            s += "║" + "  ";
-        }
-
-        s += "\n" + "╚";
-
-        for (int i = 0; i < width; i++)
-        {
-            s += "══";
-
-            if (i == width - 1)
-            {
-                s += "╝" + "\n";
-            } else
-            {
-                s += "╩";
-            }
-        }
-
-        Console.Write(s);
-    }
 };
-
-class Board
-{
-    private int size;
-
-    Board(int size)
-    {
-        this.size = size;
-    }
-}
-
