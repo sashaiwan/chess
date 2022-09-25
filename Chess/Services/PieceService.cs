cb59de5ae16f528b1a12fb7992a4d6180e5d5fb9
sashiwan <samueliwanczuk@gmail.com>
2022-09-25 14:33:32 -0300

    add services folder

---
+++ Chess/Services/PieceService.cs
@@ -0,0 +1,54 @@
+﻿using Chess.Piece;
+
+namespace Chess.Services
+{
+    public class PieceService
+    {
+        public List<(PieceColor, PieceName)> Pieces { get; set; }
+
+        public PieceService()
+        {
+        }
+
+        public void AddPiecesToSet()
+        {
+            Pieces = new List<(PieceColor, PieceName)>();
+
+            for (int i = 0; i < 8; i++)
+            {
+                Pieces.Add((PieceColor.white, PieceName.pawn));
+                Pieces.Add((PieceColor.black, PieceName.pawn));
+
+                if (i == 7)
+                {
+
+                    Pieces.Add((PieceColor.white, PieceName.rook));
+                    Pieces.Add((PieceColor.white, PieceName.rook));
+                    Pieces.Add((PieceColor.black, PieceName.rook));
+                    Pieces.Add((PieceColor.black, PieceName.rook));
+
+                    Pieces.Add((PieceColor.white, PieceName.knight));
+                    Pieces.Add((PieceColor.white, PieceName.knight));
+                    Pieces.Add((PieceColor.black, PieceName.knight));
+                    Pieces.Add((PieceColor.black, PieceName.knight));
+
+                    Pieces.Add((PieceColor.white, PieceName.bishop));
+                    Pieces.Add((PieceColor.white, PieceName.bishop));
+                    Pieces.Add((PieceColor.black, PieceName.bishop));
+                    Pieces.Add((PieceColor.black, PieceName.bishop));
+
+                    Pieces.Add((PieceColor.white, PieceName.queen));
+                    Pieces.Add((PieceColor.black, PieceName.queen));
+                    Pieces.Add((PieceColor.white, PieceName.king));
+                    Pieces.Add((PieceColor.black, PieceName.king));
+                }
+            }
+        }
+
+        public List<(PieceColor, PieceName)> GetPiecesList()
+        {
+            return Pieces;
+        }
+    }
+}
+
---
+++ Chess/Services/PositionController.cs
@@ -0,0 +1,38 @@
+﻿using Chess.Board;
+
+namespace Chess.Services
+{
+    public class PositionController
+    {
+        private BoardBase _board;
+        private PieceService _piecesSet;
+        private List<(int, int)> _positionsSet = new List<(int, int)>();
+
+        public PositionController(BoardBase board, PieceService piecesSet)
+        {
+            _board = board;
+            _piecesSet = piecesSet;
+        }
+
+        private void CreateRandomPositons()
+        {
+            _positionsSet = _board.GetAllSquares();
+            Random rnd = new Random();
+
+            for (int i = 0; i < (_positionsSet.Count - 1); i++)
+            {
+                var key = i + rnd.Next(_positionsSet.Count - i);
+                var t = _positionsSet[key];
+                _positionsSet[key] = _positionsSet[i];
+                _positionsSet[i] = t;
+            }
+        }
+
+        public List<(int, int)> GetPositionSet()
+        {
+            CreateRandomPositons();
+            return _positionsSet;
+        }
+    }
+}
+
