43d68cc144a80aea57f49bc6681057625bea39c4
sashiwan <samueliwanczuk@gmail.com>
2022-09-25 14:34:59 -0300

    add interface for piece types

---
+++ Chess/Piece/IPieceType.cs
@@ -0,0 +1,27 @@
+﻿namespace Chess.Piece
+{
+    public interface IPieceType
+    {
+        PieceColor Color { set; get; }
+        PieceName Name { set; get; }
+
+        int GetPieceType();
+    }
+
+    public enum PieceColor
+    {
+        white = 1,
+        black = -1,
+    }
+
+    public enum PieceName
+    {
+        pawn = 2,
+        rook,
+        knight,
+        bishop,
+        queen,
+        king,
+    }
+}
+
